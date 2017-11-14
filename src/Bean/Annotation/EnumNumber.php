3a738409ec6ac9d1275fc418d8e7a733aff8788f
lilin <794774870@qq.com>
2017-11-14 21:12:57 +0800

    注解参数

---
+++ src/Bean/Annotation/EnumNumber.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Swoft\Bean\Annotation;
+
+/**
+ * 非负整数枚举验证
+ *
+ * @Annotation
+ * @Target("METHOD")
+ *
+ * @uses      EnumNumber
+ * @version   2017年11月13日
+ * @author    stelin <phpcrazy@126.com>
+ * @copyright Copyright 2010-2016 swoft software
+ * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ */
+class EnumNumber
+{
+    /**
+     * 字段名称
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * 枚举值集合
+     *
+     * @var array
+     */
+    private $values;
+
+    /**
+     * 默认值，如果是null，强制验证参数
+     *
+     * @var null|int
+     */
+    private $default = null;
+
+    /**
+     * EnumStr constructor.
+     *
+     * @param array $values
+     */
+    public function __construct(array $values)
+    {
+        if (isset($values['name'])) {
+            $this->name = $values['name'];
+        }
+        if (isset($values['values'])) {
+            $this->values = $values['values'];
+        }
+        if (isset($values['default'])) {
+            $this->default = $values['default'];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValues(): array
+    {
+        return $this->values;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDefault()
+    {
+        return $this->default;
+    }
+}
---
+++ src/Bean/Annotation/Flt.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Swoft\Bean\Annotation;
+
+/**
+ * 浮点数验证
+ *
+ * @Annotation
+ * @Target("METHOD")
+ *
+ * @uses      Flt
+ * @version   2017年11月13日
+ * @author    stelin <phpcrazy@126.com>
+ * @copyright Copyright 2010-2016 swoft software
+ * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ */
+class Flt
+{
+    /**
+     * 字段名称
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * 最小值
+     *
+     * @var float
+     */
+    private $min;
+
+    /**
+     * 最小值
+     *
+     * @var float
+     */
+    private $max;
+
+    /**
+     * 默认值，如果是null，强制验证参数
+     *
+     * @var null|float
+     */
+    private $default = null;
+
+    /**
+     * Integer constructor.
+     *
+     * @param array $values
+     */
+    public function __construct(array $values)
+    {
+        if (isset($values['name'])) {
+            $this->name = $values['name'];
+        }
+        if (isset($values['min'])) {
+            $this->min = $values['min'];
+        }
+        if (isset($values['max'])) {
+            $this->max = $values['max'];
+        }
+        if (isset($values['default'])) {
+            $this->default = $values['default'];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMin(): float
+    {
+        return $this->min;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMax(): float
+    {
+        return $this->max;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getDefault()
+    {
+        return $this->default;
+    }
+}
