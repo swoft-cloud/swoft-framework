1c93cf821b29a2ccff39234cfdda945039a86869
lilin <794774870@qq.com>
2017-12-30 17:18:23 +0800

    新增注解

---
+++ src/Bean/Annotation/CacheEvict.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Swoft\Bean\Annotation;
+
+/**
+ * the annotatioin of cache evict
+ *
+ * @Annotation
+ * @Target("METHOD")
+ *
+ * @uses      CacheEvict
+ * @version   2017年12月30日
+ * @author    stelin <phpcrazy@126.com>
+ * @copyright Copyright 2010-2016 swoft software
+ * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ */
+class CacheEvict
+{
+
+}
