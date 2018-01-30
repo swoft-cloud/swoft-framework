ebcfaa270bf8e7556f8f358968e7d5fc753989ae
lilin <794774870@qq.com>
2018-01-30 23:21:31 +0800

    debug

--- src/Bean/Resource/AnnotationResource.php
+++ src/Bean/Resource/AnnotationResource.php
@@ -126,7 +126,6 @@ abstract class AnnotationResource extends AbstractResource
     public function parseBeanAnnotations(string $className)
     {
         if (!class_exists($className) && !interface_exists($className)) {
-            var_dump($className);
             return null;
         }
 
@@ -136,6 +135,7 @@ abstract class AnnotationResource extends AbstractResource
         $reflectionClass  = new \ReflectionClass($className);
         $classAnnotations = $reader->getClassAnnotations($reflectionClass);
 
+        var_dump($className, $classAnnotations);
         // 没有类注解不解析其它注解
         if (empty($classAnnotations)) {
             return;
